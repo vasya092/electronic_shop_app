28dc95280ebfc262a4547d5e64ce300ce786a4ac
Vasya092 <vasya092@gmail.com>
2022-11-16 12:53:05 +0500

    Исправление ошибки в названиее класса

--- app/src/main/java/com/example/electronicshop/data/ProductsRepositoryImpl.kt
+++ app/src/main/java/com/example/electronicshop/data/ProductsRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.example.electronicshop.data
 import com.example.electronicshop.data.network.ProductsListResponse
 import com.example.electronicshop.data.network.ShopApi
 
-class ProductsRepostoryImpl(): ProductsRepository{
+class ProductsRepositoryImpl(): ProductsRepository{
     override suspend fun getAllProducts(): ProductsListResponse {
         return ShopApi.retrofitService.getProducts()
     }
--- app/src/main/java/com/example/electronicshop/di/ShopModule.kt
+++ app/src/main/java/com/example/electronicshop/di/ShopModule.kt
@@ -1,12 +1,12 @@
 package com.example.electronicshop.di
 
 import com.example.electronicshop.data.ProductsRepository
-import com.example.electronicshop.data.ProductsRepostoryImpl
+import com.example.electronicshop.data.ProductsRepositoryImpl
 import dagger.Module
 import dagger.Provides
 
 @Module
 class ShopModule {
     @Provides
-    fun provideProductsRepository() : ProductsRepository = ProductsRepostoryImpl()
+    fun provideProductsRepository() : ProductsRepository = ProductsRepositoryImpl()
 }
