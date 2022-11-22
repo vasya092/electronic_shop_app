3b5bdeb846b1ee6b8d87dde7e892650c4d094796
Vasya092 <vasya092@gmail.com>
2022-11-22 22:08:05 +0500

    Получение валюты в зависимости от настроек

--- app/src/main/java/com/example/electronicshop/ui/adapter/BestSellersDelegateAdapter.kt
+++ app/src/main/java/com/example/electronicshop/ui/adapter/BestSellersDelegateAdapter.kt
@@ -4,6 +4,9 @@ import coil.load
 import com.example.electronicshop.databinding.BestSellersSliderItemBinding
 import com.example.electronicshop.model.BestSellerProduct
 import com.livermor.delegateadapter.delegate.ViewBindingDelegateAdapter
+import okhttp3.internal.format
+import java.text.NumberFormat
+import java.util.*
 
 class BestSellersDelegateAdapter(
     private val clickListener: (BestSellerProduct) -> Unit):
@@ -12,10 +15,17 @@ class BestSellersDelegateAdapter(
 
     override fun BestSellerProduct.getItemId(): Any = id
 
+
+
     override fun BestSellersSliderItemBinding.onBind(item: BestSellerProduct) {
+        val numberFormat = NumberFormat.getCurrencyInstance()
+        numberFormat.maximumFractionDigits = 0
+        numberFormat.currency = Currency.getInstance("USD")
+
         bestSellerItemTitle.text = item.title
-        bestSellerItemPrice.text = item.price_without_discount.toString()
-        discountPrice.text = item.discount_price.toString()
+        bestSellerItemPrice.text = numberFormat.format(item.price_without_discount)
+        discountPrice.text = numberFormat.format(item.discount_price)
+
         bestSellerItemImage.load(item.picture)
         root.setOnClickListener {
             clickListener(item)
