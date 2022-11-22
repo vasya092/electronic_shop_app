15fa84c32f42f86df9c246803c26afb9a1895244
Vasya092 <vasya092@gmail.com>
2022-11-22 15:28:04 +0500

    Доработка детальной страницы

--- app/src/main/java/com/example/electronicshop/ui/DetailFragment.kt
+++ app/src/main/java/com/example/electronicshop/ui/DetailFragment.kt
@@ -1,11 +1,17 @@
 package com.example.electronicshop.ui
 
+import android.content.res.ColorStateList
+import android.graphics.BlendMode
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import com.example.electronicshop.BaseApplication
+import com.example.electronicshop.R
 import com.example.electronicshop.databinding.FragmentDetailBinding
 import com.example.electronicshop.ui.adapter.CarouselAdapter
 import com.example.electronicshop.ui.viewmodel.DetailProductViewModel
@@ -31,14 +37,18 @@ class DetailFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         viewModel.productDetail.observe(viewLifecycleOwner) {
             binding?.productDetailTitle?.text = it.title
             binding?.titleAdvantagesCpu?.text = it.CPU
             binding?.titleAdvantagesCamera?.text = it.camera
             binding?.titleAdvantagesSsd?.text = it.ssd
             binding?.titleAdvantagesHdd?.text = it.sd
-            binding?.selectColorButtonFirst?.isActivated = true
+
+            binding?.selectColorFirst?.backgroundTintList = ColorStateList.valueOf(Color.parseColor(it.color[0]))
+            binding?.selectColorSecond?.backgroundTintList = ColorStateList.valueOf(Color.parseColor(it.color[1]))
+
+            binding?.selectMemoryFirst?.text = resources.getString(R.string.select_memory_text_constructor, it.capacity[0])
+            binding?.selectMemorySecond?.text = resources.getString(R.string.select_memory_text_constructor, it.capacity[1])
             val adapter = CarouselAdapter(viewModel.productDetail.value?.images)
             binding?.carousel?.setAdapter(adapter)
             binding?.carousel?.refresh()
