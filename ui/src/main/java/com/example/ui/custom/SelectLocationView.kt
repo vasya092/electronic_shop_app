8855117ff762e7e7acef25e729abfa2405812f34
Vasya092 <vasya092@gmail.com>
2022-12-07 21:51:03 +0500

    Добавление view location компонента

---
+++ ui/src/main/java/com/example/ui/custom/SelectLocationView.kt
@@ -0,0 +1,14 @@
+package com.example.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.ui.R
+
+class SelectLocationView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    ConstraintLayout(context, attrs, defStyleAttr) {
+    init {
+        View.inflate(context, R.layout.view_location_view, this)
+    }
+}
