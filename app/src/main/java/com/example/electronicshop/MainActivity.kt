1a3f9f73f4210a627aaa760c0d440b0a5d80b090
Vasya092 <vasya092@gmail.com>
2022-12-08 18:30:50 +0500

    Перенос локации в тулбар, подключение кнопки фильтра, мелкие правки.

--- app/src/main/java/com/example/electronicshop/MainActivity.kt
+++ app/src/main/java/com/example/electronicshop/MainActivity.kt
@@ -1,21 +1,13 @@
 package com.example.electronicshop
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.electronicshop.databinding.ActivityMainBinding
 import com.example.electronicshop.ui.FilterBottomSheet
@@ -30,16 +22,31 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         navController = findNavController(R.id.nav_host_fragment_content_main)
         binding.toolbarRoot.toolbarActionbar.setupWithNavController(navController)
+
+        val menuItemFilter = binding.toolbarRoot.toolbarActionbar.menu.findItem(R.id.action_filter)
+
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if(destination.id == R.id.homeFragment) {
-                binding.toolbarWrapper.visibility = View.GONE
+                binding.toolbarRoot.selectLocationWrapper.visibility = View.VISIBLE
+                menuItemFilter.isVisible = true
             } else {
                 binding.toolbarWrapper.visibility = View.VISIBLE
+                binding.toolbarRoot.selectLocationWrapper.visibility = View.GONE
+                menuItemFilter.isVisible = false
             }
         }
+
+        menuItemFilter.setOnMenuItemClickListener {
+            val modalBottomSheet = FilterBottomSheet()
+            modalBottomSheet.show(supportFragmentManager, modalBottomSheet.tag)
+            true
+        }
+
         binding.toolbarRoot.toolbarActionbar.title = ""
+
         hideSystemUI()
     }
 
